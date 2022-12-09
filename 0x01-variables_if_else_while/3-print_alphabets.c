4f69e16a92aeab83cf58d8e24c0c966c203842a2
Qwellstra <lowellmwashi12@gmail.com>
2022-12-09 13:53:52 -0800

    Printing alphabets in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point of a function
+ *
+ * Return: 0 indicates success, 1 indicates otherwise
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
+		}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 Task 0 -Assigning a random variable
 Task 1-Assigning a random number to a variable
 Task 2- a script that prints the alphabet in lowercase
+Printing alphabets in uppercase and lowecase
