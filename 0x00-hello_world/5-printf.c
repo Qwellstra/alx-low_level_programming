2512d7e2a08f2658698655679e1927cbdf4e15aa
Qwellstra <lowellmwashi12@gmail.com>
2022-12-09 04:07:02 -0800

    Printing a statement

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("\ with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
 }
