e68c495fb77f2cb059d565ac12f827ce8a248969
Qwellstra <lowellmwashi12@gmail.com>
2022-12-09 03:20:15 -0800

    Printing a statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /**
  * puts-Printing a statement with puts
- * Return-Returns 0
+ * Return: 1 if an integer. 0 otherwise
  * int-This a string of statement
  */
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
 }
