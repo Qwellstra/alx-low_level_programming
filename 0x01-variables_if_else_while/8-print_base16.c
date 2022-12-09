02b90158d7a3e296cfa2874b5f4dab83942a2250
Qwellstra <lowellmwashi12@gmail.com>
2022-12-09 15:48:54 -0800

    Printing in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry to the function
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -6,3 +6,4 @@ A script that prints talphabets in lowercase
 A program that prints all single digit numbers in base10-Task 5
 Task 6-A script to print all the numbers from zero to ten
 A script to print alphabets in reverse
+Task 8-A program that all the numbers in base16
