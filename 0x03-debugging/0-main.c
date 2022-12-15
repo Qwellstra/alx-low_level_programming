13fa8cdda7fb300813e8044743816fadaf9263bb
Qwellstra <lowellmwashi12@gmail.com>
2022-12-15 15:55:37 -0800

    Commenting out a loop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
-
 /**
  * main - Testing a function on whether it is positive or negative
  *
  * Return: 0 for success
  */
+
 int main(void)
 {
 	int i;
-
 
 	i = 0;
 	positive_or_negative(i);
