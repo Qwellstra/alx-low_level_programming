bbad4444efec08879de3cc5c4d880960f8db5b38
Qwellstra <lowellmwashi12@gmail.com>
2022-12-15 15:30:36 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * main - Testing a function on whether it is positive or negative
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Task 0-testing whether positive or negative
