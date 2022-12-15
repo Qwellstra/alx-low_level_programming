7d456bbbd8dd37639b31a74036c277bbc4b7c8de
Qwellstra <lowellmwashi12@gmail.com>
2022-12-15 13:00:53 -0800

    Printing all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d,", i);
 			else if (i == 98)
-				printf("%d \ n", i);
+				printf("%d\n", i);
 		} else if (n >= 98)
 		{
 			for (j = n; j >= 98; j--)
@@ -27,7 +27,7 @@ void print_to_98(int n)
 				if (j != 98)
 					printf("%d,", j);
 				else if (j == 98)
-					printf("%d \ n", j);
+					printf("%d\n", j);
 			}
 		}
 	}
