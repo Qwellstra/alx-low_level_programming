e0c03a2206ba6a835b480cd04f34ee4c3c691140
Qwellstra <lowellmwashi12@gmail.com>
2023-07-31 00:51:26 -0700

    Multiplying and adding two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @x: The first integer
+ * @y: The second integer
+ * Return: Returns the addition of multiplication
+ */
+
+int mul(int x, int y)
+{
+	return(x*y);
+}
