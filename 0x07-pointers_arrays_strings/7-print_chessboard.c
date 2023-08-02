79e383845c4eb9e4be79b455a34339b7f01608a2
Qwellstra <lowellmwashi12@gmail.com>
2023-08-02 05:52:54 -0700

    A function to print a chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - Function entry point
+ * @a: array
+ *
+ * Retrun: 0 for success
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar('\n');
+		}
+	}
+}
