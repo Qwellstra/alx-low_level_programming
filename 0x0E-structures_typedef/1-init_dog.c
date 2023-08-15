de10fbbac99d9d478592a99d68790779842f0f58
Qwellstra <lowellmwashi12@gmail.com>
2023-08-15 04:33:41 -0700

    0x0E. C - sTRUCTURES, TYPEDEF

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib>
+#include "dog.h"
+
+/**
+ * init_dog - A funct to initialize the struct dog
+ * @d: a pointer to struct dog to initialize
+ * @name: The name to initialize
+ * @age: The age  to initialize
+ * @owner: The ownerto initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
