044ce14bcf8ec06e9bf88ac955bf0b360983db4e
Qwellstra <lowellmwashi12@gmail.com>
2023-08-16 00:30:30 -0700

    correction for task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - info about a dog 
+ * struct dog - info about a dog
  * @name: First memeber of the struct
  * @age: Second member of the struct
  * @owner: Third member of the struct
@@ -24,7 +24,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t*d);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
