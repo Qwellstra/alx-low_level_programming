49273b45b7f282c7a2c7623b15e90e60367da5ea
Qwellstra <lowellmwashi12@gmail.com>
2023-08-29 03:39:07 -0700

    Task 11 corrections

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,51 +1,25 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
+ * reverse_listint - a functionto reverse a linked list
  * @head: pointer to the first node in the list
  *
- * Return: pointer to the first node in the new list
+ * Return: the pointer to first node in the new list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL;
-    listint_t *current = *head;
-    listint_t *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-    while (current)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
 
-    *head = prev;
+	*head = prev;
 
-    return (*head);
-}
-
-int main(void)
-{
-    listint_t *head = NULL;
-
-    /* Add nodes to the linked list (you can adapt this part to your needs) */
-    add_nodeint(&head, 10);
-    add_nodeint(&head, 20);
-    add_nodeint(&head, 30);
-    add_nodeint(&head, 40);
-
-    printf("Original linked list:\n");
-    print_listint(head);
-
-    /* Reverse the linked list */
-    reverse_listint(&head);
-
-    printf("Reversed linked list:\n");
-    print_listint(head);
-
-    /* Free memory (you can adapt this part to your needs) */
-    free_listint(head);
-
-    return (0);
+	return (*head);
 }
